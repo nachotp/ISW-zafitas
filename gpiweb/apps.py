b768910d36dc0c71f6a993852e868ca39acd98b5
Ignacio Tampe <thenachotp@gmail.com>
2018-05-13 22:31:22 -0400

    fix estructura proyecto

--- gpiweb/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GpiwebConfig(AppConfig):
-    name = 'Gpiweb'
--- gpiweb/wsgi.py
+++ gpiweb/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for untitled project.
+WSGI config for gpiweb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gpi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gpiweb.settings")
 
 application = get_wsgi_application()
